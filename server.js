65c0d74ff46ec53f4a2193946107617c4fe89f96
Martin <martindallaire@outlook.com>
2020-06-13 17:59:39 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
+console.log("My socket server is running.");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+	console.log('new connection: '+ socket.id);
+	socket.on('start', startMsg);
+
+	function startMsg(data){
+		socket.broadcast.emit('start', data);
+		console.log(data);
+	}
+	socket.on('paint', paintMsg);
+
+	function paintMsg(data){
+		socket.broadcast.emit('paint', data);
+		console.log(data);
+	}
+}
